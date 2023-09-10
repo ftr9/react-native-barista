2f88aae75ce6b621a20410541eddb8084a22b6a5
ftr9 <ferlandodotel16@gmail.com>
2023-09-10 14:57:19 +0545

    Refactor: Added ICartProduct & ITeas interface in a global types

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+export interface ICartInProduct {
+  qty: number;
+  name: string;
+  image: any;
+  price: number;
+}
+
+export interface ITeas {
+  name: string;
+  price: number;
+  image: any;
+}
