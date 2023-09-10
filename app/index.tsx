0bcbb45fb9b9cd267dfe8f7edd99fb0a4dab18ee
ftr9 <ferlandodotel16@gmail.com>
2023-09-10 14:51:20 +0545

    Refactor: Removed unnecessary import

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,7 @@
-import { Image, StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import TeaList from '../components/TeaList';
 import DropPlateArea from '../components/DropPlateArea';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AddToCart from '../components/AddToCart';
 
 const MainPage = () => {
